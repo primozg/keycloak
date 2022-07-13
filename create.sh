6202572a90ac4806222c2ddd8da693bd006d4549
Primož Gorkič <primoz@gorkic.com>
2022-07-13 23:48:04 +0000

    Update README.md

---
+++ create.sh
@@ -0,0 +1,7 @@
+echo "# keycloak" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/primozg/keycloak.git
+git push -u origin main
